2880c9aaeaf98c02f7da4a5eee55f015034b5c24
Sungoh Park <spark449062@gmail.com>
2021-07-20 01:56:45 -0600

    commit tree 39cbcccc7ca265a337e9c8b489dd0a9dfe90fd84

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 3.1;
+let num2 = 20;
+let num3 = 9;
+let num4 = 5;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let mod = num3 % num4;
+let max = Math.max(num1, num2, num3, num4);
